edbb17ff636e51343c4d52f29fc8b45161a13957
alari777 <alari777@gmail.com>
2022-12-16 13:27:43 +0200

    was refactored index.js: were removed unneeded pixels of code

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
-import { XMLBuilder, XMLParser } from 'fast-xml-parser';
-import { dehydrate, QueryClient, useQuery } from 'react-query';
 import { useState, useEffect } from 'react';
-// const testFetch = async () => await (await fetch('http://localhost:3000/api/hello')).json();
 
 const Home = () => {  
-  /*
-  const { data } = useQuery('arrtest', testFetch, { refetchInterval: 2000 });
-
-  if (!data && data.violators.length > 0) {
-    return (
-      <div>No data!</div>
-    )
-  }
-  */
-
   const [violators, violatorsSet] = useState([]);
   const [snapshotTimestamp, snapshotTimestampSet] = useState()
 
@@ -29,15 +16,6 @@ const Home = () => {
           return violator;
         }
       });
-
-      /*
-      const newNewViolators = newViolators.filter(violator => {
-        const subtractDatetime = (new Date() - new Date(violator.atr_snapshotTimestamp)) / 1000 / 60;
-        if (subtractDatetime < 10) {
-          return violator;
-        }
-      });
-      */
 
       violatorsSet([...pilots, ...violators]);
       snapshotTimestampSet(atr_snapshotTimestamp);
@@ -65,50 +43,3 @@ const Home = () => {
 }
 
 export default Home;
-
-/*
-export async function getServerSideProps(context) {
-  const queryClient = new QueryClient();
-
-  const result = await queryClient.prefetchQuery('arrtest', testFetch);
-
-  console.log('hello');
-
-  return {
-    props: { dehydratedState: dehydrate(queryClient) }
-  } 
-}
-*/
-
-/*
-export async function getStaticProps(context) {
-  const response = await fetch('https://assignments.reaktor.com/birdnest/drones');
-  const result = await response.text();
-
-  const options = {
-    ignoreAttributes: false,
-    attributeNamePrefix: 'atr_',
-  };
-
-  const parser = new XMLParser(options);
-  const drones = parser.parse(result);
-
-  let violators = [];
-  // if (jObj.report.capture.drone.lenght > 0) {
-  console.log(drones.report.capture.atr_snapshotTimestamp);
-
-  violators = drones.report.capture.drone.filter((dron) => {
-    const hypot = Math.sqrt(
-      Math.pow(250 * 100 * 10 - dron.positionX, 2) +
-        Math.pow(250 * 100 * 10 - dron.positionY, 2),
-    );
-    if (hypot < 100 * 100 * 10) {
-      return dron;
-    }
-  });
-  // console.log(violators);
-  return {
-    props: { violators }, // will be passed to the page component as props
-  }
-}
-*/
