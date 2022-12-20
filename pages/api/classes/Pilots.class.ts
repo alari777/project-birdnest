3164391b565d415b7641cd1bdf5f33f572d7e77c
alari777 <alari777@gmail.com>
2022-12-20 15:02:34 +0200

    stable working version, #3-2

--- pages/api/classes/Pilots.class.ts
+++ pages/api/classes/Pilots.class.ts
@@ -2,12 +2,38 @@ import { XMLParser } from 'fast-xml-parser';
 import { BootstrapType, DronesType, DroneType, ViolotarType } from '../../../types/violators.type';
 import { DistanceStatusEnum } from '../../../enums/violators.enum';
 
+export const getDrones = async(instance: Pilots): Promise<DroneType[] | undefined> => {
+  const response = await fetch('https://assignments.reaktor.com/birdnest/drones');
+  if (response.status === 200) {
+    const result = await response.text();
+    const drones: DronesType = instance.parser.parse(result);
+
+    const {drone, atr_snapshotTimestamp} = drones.report.capture;
+
+    instance.atrSnapshotTimestamp = atr_snapshotTimestamp;
+
+    const violators = drone.filter((dron: DroneType) => {
+      const hypot = Math.sqrt(
+          Math.pow(250 * 100 * 10 - dron.positionX, 2) +
+          Math.pow(250 * 100 * 10 - dron.positionY, 2),
+      );
+      if (hypot < 100 * 100 * 10) { // <- 100m * 100cm * 10mm -- that is radius of `no drone zone`
+        dron.newDistance = Math.ceil((hypot / 10 / 100) * 100) / 100;
+        return dron;
+      }
+    });
+
+    return violators;
+  }
+  return undefined;
+}
+
 export class Pilots {
   private static instance: Pilots;
 
+  public atrSnapshotTimestamp: string;
+  public parser: XMLParser;
   private map: Map<string, ViolotarType>;
-  private parser: XMLParser;
-  private atr_snapshotTimestamp: string;
 
   private constructor() {
     this.map = new Map();
@@ -26,26 +52,28 @@ export class Pilots {
 
   public bootstrap = async(): Promise<BootstrapType> => {
     try {
-      const drones = await this.getDrones();
-      await this.getViolatorsPilots(drones);
+      // const drones = await this.getDrones();
+      const drones = await getDrones(this);
+      if (drones) await this.getViolatorsPilots(drones);
+
       const pilots = this.formViolatorsPilots();
       return {
         pilots,
-        atr_snapshotTimestamp: this.atr_snapshotTimestamp
+        atr_snapshotTimestamp: this.atrSnapshotTimestamp
       };
     } catch(err) {
         console.log('bootstrap:', err.message);
     }
   }
 
-  private getDrones = async(): Promise<DroneType[]> => {
+  /*private getDrones = async(): Promise<DroneType[]> => {
     const response = await fetch('https://assignments.reaktor.com/birdnest/drones');
     const result = await response.text();
     const drones: DronesType = this.parser.parse(result);
 
     const { drone, atr_snapshotTimestamp } = drones.report.capture;
 
-    this.atr_snapshotTimestamp = atr_snapshotTimestamp;
+    this.atrSnapshotTimestamp = atr_snapshotTimestamp;
 
     const violators = drone.filter((dron: DroneType) => {
       const hypot = Math.sqrt(
@@ -59,14 +87,14 @@ export class Pilots {
     });
 
     return violators;
-  }
+  }*/
 
   private getViolatorsPilots = async(violators: DroneType[]): Promise<void> => {
     await Promise.all(violators.map(async (dron) => {
       const responsePilot = await fetch(`https://assignments.reaktor.com/birdnest/pilots/${dron.serialNumber}`);
       if (responsePilot.status === 200) {
         const resultPilot: ViolotarType = await responsePilot.json();
-        resultPilot.atr_snapshotTimestamp = this.atr_snapshotTimestamp;
+        resultPilot.atr_snapshotTimestamp = this.atrSnapshotTimestamp;
         const newDistance = dron.newDistance;
         let oldDistance = dron.newDistance;
         let previousDistance = '';
@@ -96,8 +124,8 @@ export class Pilots {
     const pilots: ViolotarType[] = [];
     for (let pilot of this.map.values()) {
       const subtractDatetime: number = Number(new Date()) - Number(new Date(pilot.atr_snapshotTimestamp));
-      const subtractDatetimeInMins = Number(subtractDatetime / 1000 / 60);
-      if (subtractDatetimeInMins > 10) {
+      const subtractTimeInMinutes = Number(subtractDatetime / 1000 / 60);
+      if (subtractTimeInMinutes > 10) {
         this.map.delete(pilot.pilotId);
       } else {
           pilots.push(pilot);
