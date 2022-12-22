f2ec8a6ceb7955aea825680bdb59a308b3f1f912
alari777 <alari777@gmail.com>
2022-12-22 11:10:35 +0200

    stable working version, #3-4

--- pages/api/classes/pilots.test.js
+++ pages/api/classes/pilots.test.js
@@ -1,36 +1,74 @@
-import { Pilots } from "./Pilots.class";
-import {ViolotarType} from "../../../types/violators.type";
-//jest.mock('./Pilots.class');
-// jest.mock('fetch');
+import { getDrones, getViolatorsPilots, Pilots } from "./Pilots.class";
+import {
+    mockXmlWithoutViolators,
+    mockXmlWithViolators,
+    mockJsonViolators,
+    mockPilot
+} from "./mock.data.pilots";
+import * as moduleApi from './Pilots.class';
 
-describe('some tests', () => {
-    it("should return hello for the first call", async () => {
-        // jest.mock('fetch');
-        const result = new Pilots();
-        result.bootstrap = jest.fn().mockReturnValue({
-            pilots: {
-                pilotId: '',
-                firstName: '',
-                phoneNumber: '',
-                email: '',
-            },
-            atr_snapshotTimestamp: ''
-        })
-        /*
-        result.getDrones = jest.fn().mockReturnValue({
-            positionX: 0,
-            positionY: 0,
-            newDistance: 0
-        });
-        const f = await result.getDrones();
-        expect(f).toEqual({
-            positionX: 0,
-            positionY: 0,
-            newDistance: 10
-        });
-        */
-
-        // const getDronesMock = jest.
-        // await result.bootstrap();
+describe('testing function getDrones()', () => {
+    beforeEach(() => {
+        fetch.resetMocks();
     });
+
+    it("should return empty array, status code is 200, there are no any violators", async () => {
+        fetch.mockResponseOnce(mockXmlWithoutViolators, {
+            status: 200,
+            headers: {'content-type': 'text/plain'}
+        });
+        const result = await getDrones();
+        expect(result).toHaveLength(0);
+    });
+
+    it("should return empty array, status code is NOT 200, there are no any violators", async () => {
+        fetch.mockResponseOnce(mockXmlWithoutViolators, {
+            status: 404,
+            headers: {'content-type': 'text/plain'}
+        });
+        const result = await getDrones();
+        expect(result).toBeUndefined();
+    });
+
+    it("should return array with length equals 3, status code is 200, there are no any violators", async () => {
+        fetch.mockResponseOnce(mockXmlWithViolators, {
+            status: 200,
+            headers: {'content-type': 'text/plain'}
+        });
+        const result = await getDrones();
+        expect(result).toHaveLength(3);
+    });
+
+    it("should return array with length equals 3, status code is NOT 200, there are no any violators", async () => {
+        fetch.mockResponseOnce(mockXmlWithViolators, {
+            status: 404,
+            headers: {'content-type': 'text/plain'}
+        });
+        const result = await getDrones();
+        expect(result).toBeUndefined();
+    });
+});
+describe('testing function getViolatorsPilots()', () => {
+    beforeEach(() => {
+        fetch.resetMocks();
+    });
+
+    it("should return void, status code is 200, map.has, map.get, map.delete", async () => {
+        fetch.mockResponse(JSON.stringify(mockJsonViolators), {
+            status: 200,
+            headers: {'content-type': 'application/json'}
+        });
+
+        Map.prototype.has = jest.fn().mockReturnValue(true);
+        Map.prototype.get = jest.fn().mockReturnValue({ distance: 1});
+        Map.prototype.delete = jest.fn();
+
+        const result = await getViolatorsPilots([mockPilot]);
+
+        expect(result).toBeUndefined();
+        expect(Map.prototype.has).toHaveBeenCalled();
+        expect(Map.prototype.get).toHaveBeenCalledTimes(2);
+        expect(Map.prototype.delete).toHaveBeenCalled();
+    });
+
 });
