045f66a2e8e161c332d0fce5ff02251eb6912737
alari777 <alari777@gmail.com>
2022-12-22 11:11:25 +0200

    stable working version, #3-5

--- pages/index.test.jsx
+++ pages/index.test.jsx
@@ -1,15 +1,32 @@
+require('jest-fetch-mock').disableMocks();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Home from '../pages/index';
+import Home from './index';
 import '@testing-library/jest-dom';
 
-jest.mock('fetch');
-
 describe('Home', () => {
-  it('renders input checkbox', () => {
-    render(<Home />)
+  let container;
 
-    const inputCheckbox = screen.getByText(/Extended View/i);
-    expect(inputCheckbox).toBeInTheDocument();
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  it('renders input checkbox', () => {
+    // act(() => {
+    //   ReactDOM.createRoot(container).render(<Home />);
+    // });
+    // // render(<Home />)
+    //
+    // const inputCheckbox = screen.getByText(/Extended View/i);
+    // expect(inputCheckbox).toBeInTheDocument();
   });
 
   // it('test fetch function', () => {
