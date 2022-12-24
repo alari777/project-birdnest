7c2c5f707e9cbcf839136618740686c2324c1e70
alari777 <alari777@gmail.com>
2022-12-24 13:02:11 +0200

    was updated class Pilot, was removed exported function named `getViolatorsPilots`

--- pages/api/classes/Pilots.class.ts
+++ pages/api/classes/Pilots.class.ts
@@ -1,39 +1,7 @@
-import { BootstrapType, DroneType, ViolatorType } from '../../../types/violators.type';
-import { DistanceStatusEnum } from '../../../enums/violators.enum';
+import { BootstrapType, ViolatorType } from '../../../types/violators.type';
 import { getDrones } from '../utils/getdrones/getDrones';
+import { getViolatorsPilots } from '../utils/getViolatorsPilots/getViolatorsPilots';
 
-
-export const getViolatorsPilots = async(violators: DroneType[]): Promise<void> => {
-  const instance = Pilots.init();
-  await Promise.all(violators.map(async (dron) => {
-    const responsePilot = await fetch(`https://assignments.reaktor.com/birdnest/pilots/${dron.serialNumber}`);
-    if (responsePilot.status === 200) {
-      const resultPilot: ViolatorType = await responsePilot.json();
-      resultPilot.atr_snapshotTimestamp = instance.atrSnapshotTimestamp;
-      const newDistance = dron.newDistance;
-      let oldDistance = dron.newDistance;
-      let previousDistance = '';
-      resultPilot.status = '';
-      resultPilot.previousDistance = '';
-      if (instance.map.has(resultPilot.pilotId)) {
-        oldDistance = Number(instance.map.get(resultPilot.pilotId).distance);
-        previousDistance = String(instance.map.get(resultPilot.pilotId).previousDistance);
-        instance.map.delete(resultPilot.pilotId);
-      }
-      if (newDistance < oldDistance) {
-        resultPilot.previousDistance = String(oldDistance);
-        resultPilot.distance = newDistance;
-        resultPilot.status = DistanceStatusEnum.UPDATE;
-      } else {
-        resultPilot.previousDistance = previousDistance;
-        resultPilot.distance = oldDistance;
-        resultPilot.status = previousDistance === '' ? '' : DistanceStatusEnum.UPDATE;
-      }
-
-      instance.map.set(resultPilot.pilotId, resultPilot);
-    }
-  }));
-}
 export const formViolatorsPilots = (): ViolatorType[] => {
   const instance = Pilots.init();
   const pilots: ViolatorType[] = [];
