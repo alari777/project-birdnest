7ea4582e88b019a33bb344d7b98db2e72c790c15
alari777 <alari777@gmail.com>
2022-12-25 19:48:25 +0200

    were added: tests for Footer and Header components

---
+++ components/header.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+it('should render header', () => {
+    render(<Header />);
+    const header = screen.getByText(/PROJECT BIRDNEST: MONITORING SYSTEM/i);
+    expect(header).toBeInTheDocument();
+    expect(header).toMatchSnapshot();
+});
