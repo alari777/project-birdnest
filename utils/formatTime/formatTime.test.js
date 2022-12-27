3e5d7a2af8d6b212f4c8fae6ddd5beb780f9f848
alari777 <alari777@gmail.com>
2022-12-27 19:15:32 +0200

    were updated: function formatTime() and test for it

--- utils/formatTime/formatTime.test.js
+++ utils/formatTime/formatTime.test.js
@@ -1,6 +1,6 @@
 import { formatTime } from './formatTime';
 
-describe('testing function formatTime()', () => {
+describe('testing function formatTime(), right values', () => {
     it("should return date in new format", () => {
         const result = formatTime('2022-12-11T19:48:16.956Z')
         expect(result).toBe('11.12.2022 19:48:16');
@@ -11,7 +11,7 @@ describe('testing function formatTime()', () => {
         expect(result).toBe('');
     });
 
-    it("should return day with zero", () => {
+    it("should return day with zero: 2022-12-05T19:48:16.956Z", () => {
         const result = formatTime('2022-12-05T19:48:16.956Z')
         expect(result).toBe('05.12.2022 19:48:16');
     });
@@ -34,5 +34,43 @@ describe('testing function formatTime()', () => {
     it("should return second with zero: 15.07.2022 19:33:04", () => {
         const result = formatTime('2022-07-15T19:33:04.956Z')
         expect(result).toBe('15.07.2022 19:33:04');
+    });
+
+});
+
+describe('testing function formatTime(), wrong values', () => {
+    it("wrong data", () => {
+        const result = formatTime('wrong data')
+        expect(result).toBe('');
+    });
+
+    it("should return empty, wrong day: AA.07.2022 19:33:04", () => {
+        const result = formatTime('2022-07-AAT19:33:04.956Z')
+        expect(result).toBe('');
+    });
+
+    it("should return empty, wrong month", () => {
+        const result = formatTime('2022-AA-15T19:33:04.956Z')
+        expect(result).toBe('');
+    });
+
+    it("should return empty, wrong year: 15.07.AAAA 19:33:04", () => {
+        const result = formatTime('AAAA-07-15T19:33:04.956Z')
+        expect(result).toBe('');
+    });
+
+    it("should return empty, wrong hour: 15.07.2022 AA:33:04", () => {
+        const result = formatTime('2022-07-15TAA:33:04.956Z')
+        expect(result).toBe('');
+    });
+
+    it("should return empty, wrong minute: 15.07.2022 19:AA:04", () => {
+        const result = formatTime('2022-07-15T19:AA:04.956Z')
+        expect(result).toBe('');
+    });
+
+    it("should return empty, wrong second: 15.07.2022 19:33:AA", () => {
+        const result = formatTime('2022-07-15T19:33:AA.956Z')
+        expect(result).toBe('');
     });
 });
