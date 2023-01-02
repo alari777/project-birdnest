fe1c3723c7d79e4ddc86e563c998690c205422e0
alari777 <alari777@gmail.com>
2023-01-02 10:47:43 +0200

    was removed comment from pilots.test file

--- pages/api/classes/pilots.test.js
+++ pages/api/classes/pilots.test.js
@@ -1,15 +1,8 @@
 import { Pilots } from "./Pilots.class";
-import { mockXmlWithoutViolators } from "./mock.data.pilots";
 
 describe('testing function bootstrap()', () => {
     it("bootstrap working", async () => {
         const instance = new Pilots();
-        // fetch.resetMocks();
-        // jest.clearAllMocks();
-        // fetch.mockResponseOnce(mockXmlWithoutViolators, {
-        //     status: 200,
-        //     headers: {'content-type': 'text/plain'}
-        // });
         const result = await instance.bootstrap();
         expect(result).toEqual({
             pilots: [],
