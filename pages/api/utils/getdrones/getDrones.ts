6b5b4804063a817f72c376ed3bb85e60b500706f
alari777 <alari777@gmail.com>
2023-01-03 20:50:50 +0200

    were added comments for `getDrone` function

--- pages/api/utils/getdrones/getDrones.ts
+++ pages/api/utils/getdrones/getDrones.ts
@@ -10,6 +10,13 @@ type GetDronesType = {
     violators: DroneType[];
     atrSnapshotTimestamp: string;
 }
+
+/*
+This function makes next ones:
+1. GET() request to third party API in order to fetch data in XML response
+2. Parse this response
+3. Use Pythagoras theorem in order to find violators drones
+*/
 export async function getDrones(): Promise<GetDronesType> {
     try {
         const response = await fetch('https://assignments.reaktor.com/birdnest/drones');
@@ -19,11 +26,14 @@ export async function getDrones(): Promise<GetDronesType> {
 
             const { drone, atr_snapshotTimestamp } = drones.report.capture;
 
+            // The filtering an object which was created via parsing XML response
             const violators = drone.filter((dron: DroneType) => {
+                // Use Pythagoras theorem in order to find a hypotenuse
                 const hypot = Math.sqrt(
                     Math.pow(250 * 100 * 10 - dron.positionX, 2) +
                     Math.pow(250 * 100 * 10 - dron.positionY, 2),
                 );
+                // If hypotenuse is less than 100 metres (100 * 100 * 10) then this pilot is added it response
                 if (hypot < 100 * 100 * 10) { // <- 100m * 100cm * 10mm -- that is radius of `no drone zone`
                     dron.newDistance = Math.ceil((hypot / 10 / 100) * 100) / 100;
                     return dron;
